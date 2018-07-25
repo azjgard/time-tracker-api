7c6eb1ec5b25fbcd531890775124d17ffb6d3ae2
Jordin Gardner <gard.jordin@gmail.com>
2018-07-24 21:49:01 -0600

    fixed route for adding timelogs

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,25 +1,38 @@
-const User = require('../models/UserModel');
-const router = require('../router');
-const {jwtProtected: authProtected} = require('../middleware');
+const User = require("../models/UserModel");
+const router = require("../router");
+const { jwtProtected: authProtected } = require("../middleware");
 
 // The user can get information about themselves if they're logged
 // in and the client has stored a JWT
-router.get('/me', authProtected, async (req, res) => {
+router.get("/me", authProtected, async (req, res) => {
   res.status(200).send(req.user);
 });
 
-// Log a new timelog under the user model
-router.post('/timelog', authProtected, async (req, res) => {
-  const {clockInTime, clockOutTime} = req.body;
-
+router.post("/timelog", authProtected, async (req, res) => {
+  const { clockInTime, clockOutTime } = req.body;
   const user = await User.findById(req.user.id);
-  user.logs.push({startTime: clockInTime, endTime: clockOutTime});
-  await user.save();
 
-  // create a new timelog under the
-  // user model
+  if (user) {
+    user.logs.push({ startTime: clockInTime, endTime: clockOutTime });
+    await user.save();
 
-  res.status(200).send({startTime: clockInTime, endTime: clockOutTime});
+    res.status(200).send({
+      message: "Time successfully logged",
+      timelog: { startTime: clockInTime, endTime: clockOutTime }
+    });
+  } else {
+    res.status(401).send({ message: "User doesnt exist" });
+  }
+});
+
+router.get("/timelog", authProtected, async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    res.status(200).send({ logs: user.logs });
+  } else {
+    res.status(401).send({ message: "User doesnt exist" });
+  }
 });
 
 module.exports = router;
